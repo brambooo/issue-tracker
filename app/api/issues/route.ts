db179d5cafde4b37097d5c195096df2e0401a3a4
Bram Reinold <bram_reinold@hotmail.com>
2023-10-19 09:47:08 +0200

    first api working

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,4 +1,5 @@
-import { NextRequest } from "next/server";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const createIssueSchema = z.object({
@@ -11,8 +12,37 @@ export async function POST(request: NextRequest) {
 
   const validation = createIssueSchema.safeParse(body);
   if (!validation.success) {
-    return request.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  //   prisma
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  console.log("newIssue", newIssue);
+
+  return NextResponse.json(newIssue, { status: 201 });
 }
+
+// import { NextRequest, NextResponse } from 'next/server';
+// import prisma from '@/prisma/client';
+// import { issueSchema } from '../../validationSchemas';
+// import { getServerSession } from 'next-auth';
+// import authOptions from '@/app/auth/authOptions';
+
+// export async function POST(request: NextRequest) {
+//   const session = await getServerSession(authOptions);
+//   if (!session)
+//     return NextResponse.json({}, { status: 401 });
+
+//   const body = await request.json();
+//   const validation = issueSchema.safeParse(body);
+//   if (!validation.success)
+//     return NextResponse.json(validation.error.format(), { status: 400 });
+
+//   const newIssue = await prisma.issue.create({
+//     data: { title: body.title, description: body.description },
+//   });
+
+//   return NextResponse.json(newIssue, { status: 201 });
+// }
